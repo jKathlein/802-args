d11eba357e0df1337adaa4d70e7e44491484b1fc
Kathlein Freire <jkathlein1@gmail.com>
2019-08-19 16:43:24 -0300

    lista impar ou par

---
+++ calculo.py
@@ -0,0 +1,8 @@
+listas = [ 1,2,3,4,5,6,7,8,9,10]
+
+for lista in listas:
+    if lista % 2 == 0:
+     print ("O número " + str(lista) + " é par")
+
+    else:
+     print("O número " + str(lista) + " é impar")
